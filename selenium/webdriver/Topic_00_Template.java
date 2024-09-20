21bbaa76916b3a997b4367edfd72b07e01ee9c7a
memele23 <lenen1050@gmail.com>
2024-09-21 06:02:29 +0700

    Initial Repo

---
+++ selenium/webdriver/Topic_00_Template.java
@@ -0,0 +1,40 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_00_Template {
+    WebDriver driver;
+
+
+    @Test
+    public void TC_01_Run_On_Firefox() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+
+    }
+
+    @Test
+    public void TC_02_() {
+
+    }
+
+    @Test
+    public void TC_03_() {
+
+    }
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+
+
+}
